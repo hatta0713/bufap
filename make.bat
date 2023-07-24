6c0718fecdb5f8b99c7860d3faef73923b707994
Masumitsu Hatta <Masumitsu.Hatta@gmail.com>
2023-07-24 16:06:38 +0900

    フォルダ構成の変更

--- make.bat
+++ make.bat
@@ -25,9 +25,9 @@ del /Q /S %RELEASE_TEMP%
 mkdir %RELEASE%
 mkdir %RELEASE_TEMP%
 
-rye run pyinstaller.exe bufap\cli\bufap-cli.spec --distpath %RELEASE_TEMP%
+rye run pyinstaller.exe src\bufap\cli\bufap-cli.spec --distpath %RELEASE_TEMP%
 
-COPY bufap\cli\bufap-getall.bat %RELEASE_TEMP%
+COPY src\bufap\cli\bufap-getall.bat %RELEASE_TEMP%
 COPY README.md %RELEASE_TEMP%
 
 pushd %RELEASE_TEMP%
